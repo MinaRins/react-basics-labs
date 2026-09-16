73fca493fef4bdfb5e79e3f0ff268be56d7efb5f
MinaRins <minniemkski@gmail.com>
2026-09-16 12:15:05 +0100

    Mark task as done

--- tasky/src/App.jsx
+++ tasky/src/App.jsx
@@ -5,24 +5,26 @@ import Task from './components/task';
 function App() {
   const [taskState, setTaskState] = useState({
     tasks: [
-      { id: 1, title: "Dishes", description: "Empty dishwasher", deadline: "Today", priority: "high" },
-      { id: 2, title: "Laundry", description: "Fold clothes and put away", deadline: "Tomorrow", priority: "low" },
-      { id: 3, title: "Tidy up", deadline: "Today", priority: "medium" }
+      { id: 1, title: "Dishes", description: "Empty dishwasher", deadline: "Today", priority: "high", done: false },
+      { id: 2, title: "Laundry", description: "Fold clothes and put away", deadline: "Tomorrow", priority: "low", done: false },
+      { id: 3, title: "Tidy up", deadline: "Today", priority: "medium", done: false }
     ]
   });
 
   return (
     <div className="container">
       <h1>Tasky</h1>
-      {taskState.tasks.map((task) => (
-        <Task 
-          key={task.id}
-          title={task.title}
-          description={task.description}
-          deadline={task.deadline}
-          priority={task.priority}
-        />
-      ))}
+      {taskState.tasks.map((task, index) => (              
+    <Task 
+      title={task.title}
+      description={task.description}
+      deadline={task.deadline}
+      key={task.id}
+      done={task.done}
+      markDone={() => doneHandler(index)}
+    />
+  ))}
+
     </div>
   );
 }
--- tasky/src/components/Task.jsx
+++ tasky/src/components/Task.jsx
@@ -5,8 +5,14 @@ const Task = (props) => {
       <p>{props.description}</p>
       <p>Due: {props.deadline}</p>
       <p>Priority: {props.priority}</p>
+      <button onClick={props.markDone}>Done</button>
+      <button onClick={props.markDone} className='doneButton'>Done</button>
+      <div className="card" style={{backgroundColor: props.done ? 'lightgrey' : '#5bb4c4'}}></div>
     </div>
+
+    
   )
+  
 }
 
 export default Task;
